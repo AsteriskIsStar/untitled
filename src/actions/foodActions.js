e7b480e1546bc8576b80f79b926eee7cf922204c
Asterisk <98jiminlee@gmail.com>
2020-06-06 16:37:31 +0900

    e

--- src/actions/foodActions.js
+++ src/actions/foodActions.js
@@ -10,7 +10,7 @@ import axios from 'axios'
 *   return state.foods.cate_info
 * }
 * */
-export function fetchFoodDetail(no) {
+/*export function fetchFoodDetail(no) {
     return function(dispatch){
         // dispatch=> 함수
         axios.get('http://localhost:3355/food_detail', {
@@ -22,4 +22,16 @@ export function fetchFoodDetail(no) {
             payload:result.data
         }))
     }
+}*/
+
+
+export const fetchFoodDetail=(no)=>dispatch=>{
+    axios.get('http://localhost:3355/food_detail', {
+        params:{
+            no:no
+        }
+    }).then((result)=>dispatch({
+        type:FETCH_FOOD_DETAIL,
+        payload:result.data
+    }))
 }
